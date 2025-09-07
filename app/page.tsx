deb46752326cffec3c29b6ee8069f3efc8684fca
jamesL103 <jameskl7337@gmail.com>
2025-09-07 12:52:33 -0400

    Add header and footer layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,6 @@
 import Image from "next/image";
 
 
-const Layout = (props) => {
-  return (
-    <>
-      <Header />
-      {props.children}
-      <Footer />
-    </>
-  )
-};
-
-function Header() {
-  return (
-    <div className="absolute top-0 bg-accent w-full p-5">
-      <p className="inline text-3xl">Lister</p>
-      <div className="inline absolute top-0 right-0">
-        <LoginButton text="Login"/>
-        <LoginButton text="Sign Up"/>
-      </div>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-function LoginButton({text}) {
-  return (
-    <button className="bg-cyan-600 m-4 px-3 py-3 hover:bg-cyan-700 active:bg-cyan-800 rounded-sm">{text}</button>
-  )
-}
-
 export default function Home() {
   return (
     /*<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -46,8 +10,8 @@ export default function Home() {
         
       </footer>
     </div>*/
-    <Layout>
-      <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-    </Layout>
+    <div>
+        <p>content</p>
+    </div>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <div className="bg-accent w-full p-5 relative">
+      <p className="text-3xl inline">Lister</p>
+      <div className="inline absolute right-0">
+        <LoginButton text="Login"/>
+        <SignupButton text="Sign Up"/>
+      </div>
+    </div>
+  )
+}
+
+function LoginButton({text}) {
+  return (
+    <Link href="/login" className="bg-cyan-600 m-4 px-3 py-3 hover:bg-cyan-700 active:bg-cyan-800 rounded-sm">{text}</Link>
+  )
+}
+
+function SignupButton({text}) {
+  return (
+    <Link href="/register" className="bg-cyan-600 m-4 px-3 py-3 hover:bg-cyan-700 active:bg-cyan-800 rounded-sm">{text}</Link>
+  )
+}
