c8e7fc37ff1c18f60253cd72356a3cad14f36c67
jamesL103 <jameskl7337@gmail.com>
2025-09-11 16:00:50 -0400

    add form functionality

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,21 +6,21 @@ export default function Login() {
         <div className="flex items-center justify-center w-full h-screen">
             <div className="object-center border-solid border-color-foreground border-2 rounded-md bg-background max-w-100 p-5 flex flex-col justify-center">
                 <h1 className="text-4xl">Login to your Account</h1>
-                <form>
-                    <TextField fieldName="Email" id="email"></TextField>
+                <form method="POST" action="/login_request" name="login">
+                    <TextField fieldName="Email" id="email" type="email"></TextField>
                     <TextField fieldName="Password" id="password"></TextField>
-                    <button type="button" className="rounded-lg bg-grape p-3 my-3">Login</button>
+                    <button type="submit" className="rounded-lg bg-grape p-3 my-3">Login</button>
                 </form>
             </div>
         </div>
     )
 }
 
-function TextField({fieldName, id}) {
+function TextField({fieldName, id, type = "text"}) {
 
     return (
         <div className="my-5">
-            <label className="text-xl">{fieldName}<br/><input type="text" id={id} required className="bg-accent text-lg w-full rounded-md"></input></label><br/>
+            <label className="text-xl">{fieldName}<br/><input type={type} id={id} name={id} required className="bg-accent text-lg w-full rounded-md"></input></label><br/>
         </div>
     )
 }
@@ -31,9 +31,10 @@ async function loginAttempt() {
     const req = {
         method: "POST",
         body: JSON.stringify({
-            
+            email: email,
+            password: pw
         })
     }
 
-    await fetch("/login_request");
+    await fetch("/login_request", req);
 }
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -5,11 +5,11 @@ export default function Register() {
         <div className="flex items-center justify-center w-full h-screen">
             <div className="object-center border-solid border-color-foreground border-2 rounded-md bg-background max-w-100 p-5 flex flex-col justify-center">
                 <h1 className="text-4xl">Create new Account</h1>
-                <form>
-                    <TextField fieldName="Email" id="email"></TextField>
+                <form method="POST" action="/register" name="register">
+                    <TextField fieldName="Email" id="email" type="email"></TextField>
                     <TextField fieldName="Password" id="password"></TextField>
                     <TextField fieldName="Confirm Password" id="confirmPw"></TextField>
-                    <button type="button" className="rounded-lg bg-grape p-3 my-3">Sign up</button>
+                    <button type="submit" className="rounded-lg bg-grape p-3 my-3">Sign up</button>
                 </form>
             </div>
         </div>
@@ -17,11 +17,11 @@ export default function Register() {
 }
 
 
-function TextField({fieldName, id}) {
+function TextField({fieldName, id, type = "text"}) {
 
     return (
         <div className="my-5">
-            <label className="text-xl">{fieldName}<br/><input type="text" id={id} className="bg-accent text-lg w-full rounded-md"></input></label><br/>
+            <label className="text-xl">{fieldName}<br/><input type={type} id={id} className="bg-accent text-lg w-full rounded-md"></input></label><br/>
         </div>
     )
 }
