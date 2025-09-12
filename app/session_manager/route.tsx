b5f97369bee2f9bc1fc9e72dfc86f43e0703c0e5
jamesL103 <jameskl7337@gmail.com>
2025-09-12 14:36:29 -0400

    Switch to promise mysql API, add server to app sync handling

--- app/session_manager/route.tsx
+++ app/session_manager/route.tsx
@@ -1,44 +1,64 @@
 import type { NextRequest } from "next/server";
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import crypto from "crypto"
 
-const connection = mysql.createConnection({
+const connection = await mysql.createConnection({
   user: "root",
   host: "localhost",
   database: "lister",
 })
 
-connection.connect((err) => {
-  if (err){
-    throw err;
-  }
-  console.log("connected to mysql")
-})
+
+console.log("connected to mysql")
 
 
 export async function POST(req: NextRequest) {
 
-    const {operation}:{operation:string} = await req.json();
+  const data = await req.formData();
 
-    if (operation == "create"){
-      const id = await createId();
-      console.log(`Creating id ${id}`)
-      return Response.json({id}, {status: 200, statusText: "OK"})
+  console.log(data)
+
+  const body = Object.fromEntries(data.entries());
+
+  if (body.operation == "create"){
+    const id = await createId();
+    console.log(`Creating id ${id}`)
+    return Response.json({id}, {status: 200, statusText: "OK"})
+  } else if (body.operation == "connect") {
+
+    if (!body.hasOwnProperty("id")) {
+      return Response.json({}, {status:400, statusText:"Bad Request"})
     }
-    
-    return Response.json({}, {status: 500, statusText: "Server Error"})
+
+    const id = Number(body.id);
+
+    return appSyncResponse(id)
+  }
+  
+  return Response.json({}, {status: 500, statusText: "Unrecognized Operation"})
 }
 
 async function createId(): Promise<number> {
     return new Promise((resolve) => {
-      connection.query("SELECT COUNT(*) FROM sessions", (err, res) => {
+      connection.query("SELECT COUNT(*) AS count FROM sessions", (err, res) => {
         if(err) {
             throw err;
         }
-        console.log(res)
+        const index = res[0].count;
         const sessionId = crypto.randomInt(999999999999)
         resolve(sessionId);
       });
     })
     
+}
+
+async function appSyncResponse(sessionId: number) {
+  const [entry] = await connection.query(`SELECT list FROM sessions WHERE session = ${sessionId} LIMIT 1`);
+  console.log(`Retrieving list from session ${sessionId}`)
+  if (entry.length == 0) {
+    console.log(`No list found for session ${sessionId}`)
+    return Response.json({}, {status:400, statusText:"Invalid session id"})
+  }
+  console.log(`Sending list to session ${sessionId}`)
+  return Response.json(entry[0])
 }
