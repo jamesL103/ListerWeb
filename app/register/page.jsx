208c32c5296af2c47138857f34a59078e5efbd95
jamesL103 <jameskl7337@gmail.com>
2025-09-25 18:48:44 -0400

    remove state indicator

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -13,7 +13,6 @@ export default function Register() {
         <div className="flex items-center justify-center w-full h-screen">
             <div className="object-center border-solid border-color-foreground border-2 rounded-md bg-background max-w-100 p-5 flex flex-col justify-center">
                 <h1 className="text-4xl">Create new Account</h1>
-                {state}
                 <form action={formAction} name="register">
                     <TextField fieldName="Email" id="email" type="email" name="email" value={email} setter={setEmail}></TextField>
                     <div className="text-red-100">{state == status.EMAIL_USED_ERROR ? "Email already in use" : ""}</div>
