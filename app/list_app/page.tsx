dfb8ec755af701bb569017e94f090575f6451f03
jamesL103 <jameskl7337@gmail.com>
2025-09-26 00:08:31 -0400

    Change to jsx

---
+++ app/list_app/page.tsx
@@ -0,0 +1,17 @@
+import {ListNode} from "../../list_components/list_node"
+
+export default function Lister() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function ListView(name:string, items:Array<ListNode>) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
